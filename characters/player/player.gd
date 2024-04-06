54d13ef9eaf4bcee9c110415c3fe5fe9c1e2920b
RedSatura <111181391+RedSatura@users.noreply.github.com>
2024-04-06 21:11:04 +0800

    add enemy

---
+++ characters/player/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var SPEED = 200
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	
+	move_and_slide()
+	
+func _unhandled_input(event):
+	velocity.x = Input.get_axis("move_left", "move_right")
+	velocity.y = Input.get_axis("move_up", "move_down")
+	
+	velocity = velocity.normalized()
+	
+	velocity *= SPEED
